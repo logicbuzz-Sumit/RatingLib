b5e04fcee306558c283bf883b13da899901f76bf
Admin <logicbuzzinfotech09@gmail.com>
2026-03-17 13:05:21 +0530

    Use Java 17 on JitPack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.logicbuzz-Sumit.RatingLib:iconratingbar:1.0.3")
+    implementation("com.github.logicbuzz-Sumit:RatingLib:1.0.4")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
